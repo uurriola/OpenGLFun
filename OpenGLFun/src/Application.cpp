9c53e6b382c2563c879cc8a4affc8d123ba8d6a6
uurriola <contact@ulrichurriola.com>
2023-03-05 16:32:21 +0100

    feat: add GLEW

--- OpenGLFun/src/Application.cpp
+++ OpenGLFun/src/Application.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -18,6 +21,11 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error! " << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
